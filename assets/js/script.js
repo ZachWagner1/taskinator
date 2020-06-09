712e8de940112aa0990b19032a4ceb3a98c57e23
ZachWagner1 <zwag14@yahoo.com>
2020-06-09 15:03:31 -0500

    new task code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
 
-    //create new item
+    // create list item
     var listItemEl = document.createElement("li");
-    //style the item
     listItemEl.className = "task-item";
-    //add the text
-    listItemEl.textContent = "this is a new task."
-    //append element to task list
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 formEl.addEventListener("submit", createTaskHandler);
